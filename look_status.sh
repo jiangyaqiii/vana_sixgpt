49a1f0dc400655c185c45a5f0715b93e1f0145a6
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-22 22:00:51 +0800

    Update look_status.sh

--- look_status.sh
+++ look_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $(docker ps -qf name=ocean-node) ]]; then
-    echo "ocean正在运行"
+if [[ $(docker ps -qf name=sixgpt-ollama-1) ]]; then
+    echo "vana-sixgpt正在运行"
 else
     echo "停止"
 fi
